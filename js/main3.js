c1dc3c3461397c37e3977001aeafb5b41886ee1d
João Gabriel Costa de Sousa <102295364+Gabriel-Costa1603@users.noreply.github.com>
2022-06-03 10:01:25 -0300

    Add files via upload

---
+++ js/main3.js
@@ -0,0 +1,109 @@
+const form = document.querySelector('#form');
+const tabela = document.querySelector('#tbody');
+let idx = form.idx.value;
+
+
+const armazenarLocalStorage = (produtos) => { localStorage.setItem('armazenados', JSON.stringify(produtos)) }
+
+
+const recuperarLocalStorage = () => JSON.parse(localStorage.getItem('armazenados') || '[]');
+
+let userid = Number(sessionStorage.getItem('logado'));
+const session = localStorage.getItem("session");
+logadoOunao();
+function logadoOunao() {
+    if (session) {
+        sessionStorage.setItem("log", session);
+        userid = session;
+    } if (!userid) {
+        window.location.href = "login.html"
+        return;
+    }
+
+}
+console.log(userid);
+
+const armazenarProduto = (savedProduto) => {
+    savedProduto.preventDefault()
+    const produto = form.produto.value;
+    const valor = Number(form.valor.value);
+    const prime = form.prime.checked;
+
+    if (idx == 'novo') {
+        const produtos = recuperarLocalStorage();
+        produtos.push({ id: produtos.length + 1, produto, valor, prime });
+        armazenarLocalStorage(produtos);
+        preencheTabela();
+        form.reset();
+    } else {
+        let produtoedit = { id: idx, produto, valor, prime }
+
+        updateProduto(idx, produtoedit);
+        preencheTabela();
+        form.reset();
+        idx = 'novo';
+    }
+}
+
+const preencheTabela = () => {
+    const produtos = recuperarLocalStorage();
+    tabela.innerHTML = '';
+    for (const produto of produtos) {
+        tabela.innerHTML += `
+        <tr>
+            <th scope="row">${produto.id}></th>
+            <td>${produto.produto}</td>
+            <td>${produto.valor}</td>
+            <td>${produto.prime ? "Sim" : "Não"}</td>
+            <td>
+                <img type="button" width="40" src="img/lixo.jpg" onclick="deletProduto(${produto.id})">
+                <img type="button" width="40" src="img/lapis.jpg" onclick="editProduto(${produto.id})">
+            </td>
+        </tr>    
+        `
+    }
+}
+
+const deletProduto = (id) => {
+    const produtos = recuperarLocalStorage();
+    const indexProduto = produtos.findIndex(produto => produto.id === id)
+    if (indexProduto < 0) return;
+    produtos.splice(indexProduto, 1);
+    armazenarLocalStorage(produtos);
+    alert('O produto foi removido')
+    preencheTabela();
+}
+
+const editProduto = (id) => {
+    const produtos = recuperarLocalStorage();
+    const indexProduto = produtos.findIndex((produto) => produto.id === id)
+    form.produto.value = produtos[indexProduto].produto;
+    form.valor.value = produtos[indexProduto].valor;
+    form.prime.checked = produtos[indexProduto].prime;
+    idx = id;
+}
+
+const updateProduto = (id, produtoedit) => {
+    const produtos = recuperarLocalStorage();
+    const indexProduto = produtos.findIndex((produtoedit) => produtoedit.id === id);
+    produtos[indexProduto] = produtoedit;
+    armazenarLocalStorage(produtos);
+}
+
+form.addEventListener('submit', armazenarProduto);
+document.addEventListener('DOMContentLoaded', preencheTabela);
+
+
+let s = document.querySelector('#r');
+
+s.addEventListener('click', function(){
+    saindo()
+});
+function saindo(){
+    sessionStorage.removeItem("logado");
+    localStorage.removeItem("userLogado");
+    window.location.href = "login.html";
+}
+
+
+
